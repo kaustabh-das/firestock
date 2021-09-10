ac96280c32b980ab64f5bdb6d9f35f66e09ca006
Kaustabh Kumar Das <kaustabhkumar1997@gmail.com>
2021-09-10 15:07:42 +0530

    Update Title.js

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>FireStock</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Welcome User, to add more image click the button below.</p>
     </div>
   );
 };
